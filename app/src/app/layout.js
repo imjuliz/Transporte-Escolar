2c9569d9b37f224c7a0189f6a0e53734e389c28e
mebdrey <mebdelrey@gmail.com>
2025-05-16 16:41:32 -0300

    ds

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossOrigin="anonymous" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
       <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet"></link>
@@ -15,8 +15,9 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Footer/>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossOrigin="anonymous"></script>
       </body>
-       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+      
     </html>
   );
 }
--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -49,8 +49,50 @@ export default function Home() {
             img: './img/sobre3.svg',
             text: 'Não somos apenas um sistema. Somos a ponte entre a segurança e a inovação no transporte escolar.'
         }
+    ];
+const banner = [
+    {
+        img1: './img/banner1/montanha.svg',
+        img2: './img/banner1/onibus.svg',
+        img3:'./img/banner1/arvore1.svg',
+        img4: './img/banner1/arvore2.svg',
+        img5:'./img/banner1/circulo.svg'
+    }
+]
+    const card4 = [
+        {
+            img1: './img/sessao4/bolinha1.svg',
+            img2: './img/sessao4/card4_1.svg',
+            text: 'Ser de Monte Azul (?)'
+        },
+        {
+            img1: './img/sessao4/bolinha2.svg',
+            img2: './img/sessao4/card4_2.svg',
+            text: 'Entre na conta com base no seu tipo de usuario'
+        },
+        {
+            img1: './img/sessao4/bolinha3.svg',
+            img2: './img/sessao4/card4_3.svg',
+            text: 'Tenha total controle na palma da mão.'
+        }
     ]
-
+    const cards5 = [
+        {
+            img: './img/sessao5/icon1.svg',
+            title: 'Gestão inteligente',
+            text: 'Acompanham ento em tempo real o trajeto do transporte escolar, garantindo mais segurança e tranquilidade.'
+        },
+        {
+            img: './img/sessao5/icon2.svg',
+            title: 'comunicação eficiente',
+            text: 'Notificações instantâneas sobre atrasos, mudanças de rota e horários, evitando preocupações e incertezas.'
+        },
+        {
+            img: './img/sessao5/icon3.svg',
+            title: 'Previsibilidade e controle',
+            text: 'Dados acessíveis para planejamento preciso, permitindo ajustes rápidos e garantindo pontualidade.'
+        }
+    ]
     return (
         <>
             {/* navbar */}
@@ -67,7 +109,6 @@ export default function Home() {
                     <li className='login'><a href='#'>entrar</a></li>
                 </ul>
             </header>
-
             {/* banner */}
             <section className='banner'>
                 <div className='banner-container'>
@@ -75,7 +116,19 @@ export default function Home() {
                         <h1>Conectando trajetos, garantindo segurança!</h1>
                         <p>Segurança e eficiência no transporte escolar. Seu caminho para a tranquilidade começa aqui!</p>
                     </div>
-                    <div className='banner-imgs'></div>
+                    <div className='banner-imgs'>
+                    {banner.map(({ img1, img2, img3,img4, img5, card }) => (
+                            <div className="banner" key={card}>
+                                <div className='banner1img'>
+                                    <img src={img1} className='montanha' />
+                                    <img src= {img2} className='onibus'/>
+                                    <img src={img3} className='arvore1'/>
+                                    <img src={img4} className='arvore2'/>
+                                    <img src={img5} className='circulo'/>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </section>
 
@@ -127,18 +180,43 @@ export default function Home() {
                 <section className='sessão3'>
                     <h1 className='title'>Sobre nós</h1>
                     <div className='sobrenos'>
-                        {sobre.map((sobre, index) => (
-                            <div key={index}>
-                                <img src={sobre.img} />
-                                <p >{sobre.text}</p>
-                            </div>
-                        ))}
+                    {/* parte da julia */}
                     </div>
                 </section>
                 <section className='sessão4'>
                     <h2 className='title2'>Como funciona?</h2>
                     <h1 className='title'>Entenda em poucos passos</h1>
                     <h3 className='title3'>Em apenas algumas etapas fáceis, você está pronto para gerenciar suas viagens com nosso transporte escolar. Tudo me um só lugar.</h3>
+                    <div className='cards4'>
+                        {card4.map(({ img1, img2, text, card }) => (
+                            <div className="cards42" key={card}>
+                                <div className='cards42'>
+                                    <img src={img2} className='card4' />
+                                    <img src={img1} className='bolinha' />
+                                    <p className='text4'>{text}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+                <section className='sessão5'>
+                    <h2 className='title5_2'>Por que usar o -nome-?</h2>
+                    <h1 className='title5_1'>Simples, fácil e rápido</h1>
+                    <h3 className='title5_3'>Nossa plataforma te ajuda a ter controle total e otimizar uma experiência organizada e confiável com o transporte escolar!</h3>
+                    <div className='cards5'>
+                        {cards5.map(({ img, title, text, card }) => (
+                            <div className="cards52" key={card}>
+                                <div className='cards52'>
+                                    <img src={img} className='card5' />
+                                    <div className='texto5'>
+                                    <p className='title5'>{title}</p>
+                                    <p className='text5'>{text}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                       
+                    </div> <img src='./img/sessao5/Content.svg' className='rounded float-end banner2'/>
                 </section>
             </main>
 
