1a9155c0219f20e7666236bdbecc4d857f0b50f7
mebdrey <mebdelrey@gmail.com>
2025-05-24 12:56:33 -0300

    fds

--- app/postcss.config.mjs
+++ app/postcss.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// });
+// module.exports = { 
+//   plugins: [
+//     'postcss-import',
+//     'tailwindcss/nesting',
+//     'tailwindcss',
+//     'autoprefixer',
+//   ] 
+//   }
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixador: {},
+    },
+  };
