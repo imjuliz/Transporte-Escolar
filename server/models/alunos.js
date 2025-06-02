9be9d9c8ffd2a21e9f0ad869f6ed4ba208ecc63a
hirowski <blue.hiro17@gmail.com>
2025-06-02 17:16:25 -0300

    mensagens

--- server/models/alunos.js
+++ server/models/alunos.js
@@ -3,7 +3,7 @@ import { readAll2 } from "../config/database.js";
 const verAlunos = async()=>{
     try{
         const where =  " escola_id=1 " ;
-        return await readAll2('alunos ', where , 'nome, email, ponto_embarque_id' )
+        return await readAll2('alunos ', where , 'nome, escola_id, email, ponto_embarque_id' )
     }catch(err){
         console.error('Erro ao listar os alunos!!!')
         throw err;
