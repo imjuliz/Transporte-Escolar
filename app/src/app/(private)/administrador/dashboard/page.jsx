509a1d7f33567ae28a443b5f4426ab838b665fc3
mebdrey <mebdelrey@gmail.com>
2025-06-04 14:11:40 -0300

    veiculos tabela

--- app/src/app/(private)/administrador/dashboard/page.jsx
+++ app/src/app/(private)/administrador/dashboard/page.jsx
@@ -79,7 +79,7 @@ export default function dashboard() {
             </svg>
             ),
             nome: 'Motoristas ativos',
-            informacao:
+            informacao:""
         },
         {
             img: (<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
